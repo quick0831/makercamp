531b14e00b4ae46e95ed71d2dc59f4026effa153
Quick <quick0831@gmail.com>
2023-01-17 18:21:28 +0800

    add SelfIntro.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import SelfIntro from './SelfIntro';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SelfIntro />
     </div>
   );
 }
---
+++ src/SelfIntro.jsx
@@ -0,0 +1,16 @@
+import './SelfIntro.css';
+
+function SelfIntro() {
+    return (
+        <div className="selfIntro">
+            <h2 className="selfIntroTitle">
+                About Me
+            </h2>
+            <p className="selfIntroText">
+                I code. I eat. I sleep. I think therefore I am. My name is Quick Chang. Hello world.
+            </p>
+        </div>
+    );
+}
+
+export default SelfIntro;
