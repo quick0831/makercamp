13ad17eaa8ddbaf23290e3d1092f53644fa1e006
Quick <quick0831@gmail.com>
2023-01-17 20:08:43 +0800

    add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ContactInfo from './ContactInfo';
 import Navbar from './Navbar';
@@ -6,9 +7,13 @@ import SelfIntro from './SelfIntro';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <SelfIntro />
-      <ContactInfo />
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<SelfIntro />} />
+          <Route path='/contact' element={<ContactInfo />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
