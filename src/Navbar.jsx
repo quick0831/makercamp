d03c982b180396bebd383f5a33f10139cb1c4ead
Quick <quick0831@gmail.com>
2023-01-17 18:06:57 +0800

    add Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <h1 className="myName">
+                Chun-Kuei Chang 張峻魁
+            </h1>
+        </div>
+    );
+}
+
+export default Navbar;
