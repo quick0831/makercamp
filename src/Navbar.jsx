b1b99fbbed4f59b898c16ce21e74a75647340816
Quick <quick0831@gmail.com>
2023-01-17 19:41:42 +0800

    fix code style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 function Navbar() {
     return (
         <div className="navbar">
-            <h1 className="myName">
+            <h1 className="my-name">
                 Chun-Kuei Chang 張峻魁
             </h1>
         </div>
--- src/SelfIntro.jsx
+++ src/SelfIntro.jsx
@@ -2,11 +2,11 @@ import './SelfIntro.css';
 
 function SelfIntro() {
     return (
-        <div className="selfIntro">
-            <h2 className="selfIntroTitle">
+        <div className="self-intro">
+            <h2 className="self-intro-title">
                 About Me
             </h2>
-            <p className="selfIntroText">
+            <p className="self-intro-text">
                 I code. I eat. I sleep. I think therefore I am. My name is Quick Chang. Hello world.
             </p>
         </div>
